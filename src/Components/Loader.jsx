f4c77fdbc5f8ac809c6ef7026f8704fa128ebf15
AgDelfino <agustin.delfino92@gmail.com>
2023-03-02 20:43:32 -0300

    mod(Loader.jsx): finished component

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,9 +1,30 @@
-const Loader = () => {
-  return (
-    <div>
-        SOY EL LOADER
-    </div>
-  )
-}
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect } from "react";
 
-export default Loader
+const Loader = ({ popular }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="h-screen w-full bg-black flex items-center justify-center"
+    >
+      <div className="relative flex justify-center items-center space-x-4 overflow-hidden">
+        <div className="ml-0 flex flex-col md:flex-row  items-center justify-center md:first:space-x-6 text-center">
+          <h2 className="text-2xl md:text-4xl text-white">WELCOME TO</h2>
+          <h1 className="text-6xl md:text-8xl font-thin text-emerald-400 flex items-center justify-center">
+            LITE<span className=" text-center font-semibold"> FLIX</span>
+          </h1>
+        </div>
+        <motion.div
+          initial={{ x: 0 }}
+          animate={{ x: 700 }}
+          transition={{ duration: 2 }}
+          className="absolute w-full h-full bg-black"
+        ></motion.div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Loader;
