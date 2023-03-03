4fc12225eaf28f001f771a2ae24f57e7549264cb
AgDelfino <agustin.delfino92@gmail.com>
2023-03-03 02:51:47 -0300

    navbar animated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import { FaRegBell } from "react-icons/fa";
 import { AiOutlinePlus } from "react-icons/ai";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="w-full flex justify-around md:justify-between items-center py-3">
-        <div className="md:hidden w-8 h-8 flex items-center justify-center border border-white rounded-full text-white"><AiOutlinePlus/></div>
-        <div className="flex space-x-6 items-center">
+      <nav className="w-full flex justify-around md:justify-between items-center py-3 overflow-hidden">
+        <div className="md:hidden w-8 h-8 flex items-center justify-center border border-white rounded-full text-white">
+          <AiOutlinePlus />
+        </div>
+        <motion.div
+          initial={{ x: -200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{duration: 1, delay: 1}}
+          className="flex space-x-6 items-center"
+        >
           <h2 className="text-2xl text-emerald-400 font-thin 2xl:text-4xl">
             <span className="font-semibold">LITE</span>FLIX
           </h2>
           <h2 className="text-white text-sm mt-1 hidden md:block 2xl:text-lg">
             + AGREGAR PELÍCULA
           </h2>
-        </div>
-        <div className="flex md:space-x-10 text-white justify-center items-center">
+        </motion.div>
+        <motion.div
+          initial={{ x: 200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{duration: 1, delay: 1}}
+          className="flex md:space-x-10 text-white justify-center items-center"
+        >
           <div
             id="home_made_hamb"
             className="md:flex flex-col space-y-1 hidden"
@@ -27,12 +42,15 @@ const Navbar = () => {
           <div className="hidden md:flex">
             <FaRegBell className="text-2xl 2xl:text-3xl" />
           </div>
-          <img
+          <motion.img
+          initial={{opacity: 0}}
+          animate={{opacity:1}}
+          transition={{duration: 1, delay: 2}}
             src="https://www.agdelfino.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fabout.18711f97.jpg&w=1080&q=75"
             alt=""
             className="w-10 h-10 md:w-12 md:h-12 rounded-full object-cover"
           />
-        </div>
+        </motion.div>
       </nav>
     </>
   );
