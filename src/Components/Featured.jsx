48b384de2d7facefce2e9c2380cd8b1ba4219fac
AgDelfino <agustin.delfino92@gmail.com>
2023-03-02 21:01:08 -0300

    mod(Featured.jsx): mobile responsive

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -12,13 +12,13 @@ const Featured = ({featured}) => {
         <h1 className="text-4xl text-center md:text-left md:text-6xl -ml-[6px] text-emerald-400 font-semibold uppercase tracking-[2px]">
           {featured.title}
         </h1>
-        <div className="mt-6 space-x-3 flex flex-col md:flex-row">
-          <button className="bg-zinc-600 border border-zinc-600 text-white text-sm px-8 py-2 flex items-center tracking-[2px]">
+        <div className="mt-4 md:mt-6 md:space-x-3 flex flex-col md:flex-row w-[90%] mx-auto space-y-2">
+          <button className="bg-zinc-600 border border-zinc-600 text-white text-sm px-8 py-2 flex items-center justify-center tracking-[2px]">
             {" "}
-            <BsPlay className="text-sm md:text-xl" />
+            <BsPlay className="text-xl" />
             REPRODUCIR
           </button>
-          <button className="bg-zinc-600/20 border border-white text-white text-sm px-8 py-2 flex items-center tracking-[2px] backdrop-blur-lg">
+          <button className="bg-zinc-600/20 border border-white text-white text-sm px-8 py-2 flex items-center tracking-[2px] backdrop-blur-lg justify-center">
             <AiOutlinePlus className="text-xl" />
             MI LISTA
           </button>
