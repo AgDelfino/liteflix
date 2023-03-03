9b936bd8a30bd14c25f1767b1f4dfa42050f97fa
AgDelfino <agustin.delfino92@gmail.com>
2023-03-03 03:13:18 -0300

    Flix animated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,45 +7,57 @@ const Navbar = () => {
   return (
     <>
       <nav className="w-full flex justify-around md:justify-between items-center py-3 overflow-hidden">
-        <div className="md:hidden w-8 h-8 flex items-center justify-center border border-white rounded-full text-white">
+        <motion.div 
+        initial={{rotateZ: -180, scale: 0, opacity:0}}
+        animate={{rotateZ: 180, scale: 1, opacity:1}}
+        transition={{duration: 1, delay: .5}}
+        className="md:hidden w-8 h-8 flex items-center justify-center border border-white rounded-full text-white">
           <AiOutlinePlus />
-        </div>
+        </motion.div>
         <motion.div
-          initial={{ x: -200, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
+          initial={{ y: -200, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
           viewport={{ once: true }}
-          transition={{duration: 1, delay: 1}}
+          transition={{ duration: 1, delay: 1 }}
           className="flex space-x-6 items-center"
         >
           <h2 className="text-2xl text-emerald-400 font-thin 2xl:text-4xl">
-            <span className="font-semibold">LITE</span>FLIX
+          LITE<motion.span 
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          transition={{duration: 1, delay: 2}}
+          className="font-semibold">FLIX</motion.span>
           </h2>
-          <h2 className="text-white text-sm mt-1 hidden md:block 2xl:text-lg">
+          <motion.h2 
+          initial={{x:200, opacity:0}}
+          animate={{x:0, opacity:1}}
+          transition={{duration:1, delay: 2.5}}
+          className="text-white text-sm mt-1 hidden md:block 2xl:text-lg">
             + AGREGAR PELÍCULA
-          </h2>
+          </motion.h2>
         </motion.div>
         <motion.div
           initial={{ x: 200, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           viewport={{ once: true }}
-          transition={{duration: 1, delay: 1}}
+          transition={{ duration: 1, delay: 1 }}
           className="flex md:space-x-10 text-white justify-center items-center"
         >
           <div
             id="home_made_hamb"
             className="md:flex flex-col space-y-1 hidden"
           >
-            <div className="w-[24px] h-[2px] bg-white 2xl:w-[32px] 2xl:h-[3px]"></div>
-            <div className="w-[24px] h-[2px] bg-white 2xl:w-[32px] 2xl:h-[3px]"></div>
-            <div className="w-[24px] h-[2px] bg-white 2xl:w-[32px] 2xl:h-[3px]"></div>
+            <div className="w-[24px] h-[2px] bg-white 2xl:w-[32px] 2xl:h-[3px]" />
+            <div className="w-[24px] h-[2px] bg-white 2xl:w-[32px] 2xl:h-[3px]" />
+            <div className="w-[24px] h-[2px] bg-white 2xl:w-[32px] 2xl:h-[3px]" />
           </div>
-          <div className="hidden md:flex">
+          <motion.div className="hidden md:flex">
             <FaRegBell className="text-2xl 2xl:text-3xl" />
-          </div>
+          </motion.div>
           <motion.img
-          initial={{opacity: 0}}
-          animate={{opacity:1}}
-          transition={{duration: 1, delay: 2}}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 2 }}
             src="https://www.agdelfino.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fabout.18711f97.jpg&w=1080&q=75"
             alt=""
             className="w-10 h-10 md:w-12 md:h-12 rounded-full object-cover"
